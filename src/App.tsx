70dba1e6fd2eaca293b9faccf0a289e997321696
Erik Na <erikgnaa@gmail.com>
2023-02-20 14:13:46 -0300

    add: github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Character } from "./pages/Character/Character";
 import { Home } from "./pages/Home/Home";
+import { NotFound } from "./pages/NotFound/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <section className="flex justify-center bg-gray-800 px-4">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/character/:id" element={<Character />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </section>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
